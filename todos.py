bd973f9b1c8bd458374b6920af4d6a8ecf3a10fb
Andydenike <denikeandrew@gmail.com>
2019-07-18 22:16:07 -0700

    Updated to reflect new update to master branch todos.py

--- todos.py
+++ todos.py
@@ -9,34 +9,31 @@ class TodoManager(object):
     STATUS_PENDING = 'pending'
     CATEGORY_GENERAL = 'general'
 
-    def __init__(self, base_todos_path, create_dir=True):
-        self.base_todos_path = base_todos_path
-        self.path = Path(self.base_todos_path)
+    def __init__(self, base_todos_path):     
+        self.path = Path(base_todos_path)
         if self.path.exists() and not self.path.is_dir():
-            raise ValueError('{} path is invalid'.format(base_todos_path))
-
+            raise ValueError("Path is invalid")
+            
         if not self.path.exists():
-            if not create_dir:
-                raise ValueError("{} doesn't exist".format(base_todos_path))
-            self.path.mkdir(parents=True)
+            self.path.mkdir(parents=True) 
 
     def list(self, status=STATUS_ALL, category=CATEGORY_GENERAL):
         todos = {}
         for todo_path in self.path.glob('*.json'):
             with todo_path.open('r') as fp:
                 document = json.load(fp)
-                if 'category_name' not in document or 'todos' not in document:
+                if 'category_name' not in document or 'todos' not in document: 
                     raise ValueError('Invalid JSON todo format.')
-
+                    
                 category_todos = []
-                for todo in document['todos']:
+                for todo in document['todos']: 
                     if status == self.STATUS_ALL or todo['status'] == status:
                         category_todos.append(todo)
-
+                        
                 todos[document['category_name']] = category_todos
 
-        return todos
-
+        return todos 
+        
     def new(self, task, category=CATEGORY_GENERAL, description=None,
             due_on=None):
 
@@ -51,23 +48,24 @@ class TodoManager(object):
 
         todo_file_name = '{}.json'.format(category)
         path = self.path / todo_file_name
-
+        
         todos = {
             'category_name': category.title(),
             'todos': []
         }
+        
         if path.exists():
             with path.open('r') as fp:
                 todos = json.load(fp)
-
+                
         todo = {
             'task': task,
             'description': description,
             'due_on': due_on,
             'status': self.STATUS_PENDING
         }
-
+        
         todos['todos'].append(todo)
-
+        
         with path.open('w') as fp:
             todos = json.dump(todos, fp, indent=2)
